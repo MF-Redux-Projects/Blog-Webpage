4d728def2790b0ae3323ffa1c19f95e72bf2a087
mehedifoysal <mehedifoysal8@gmail.com>
2022-08-24 23:04:33 +0600

    blog state update

--- src/redux/blog-filters/initial-state.js
+++ src/redux/blog-filters/initial-state.js
@@ -19,18 +19,6 @@ export const initialState = {
         },
         {
             id: 2,
-            title: 'How to use search engine optimization to drive sales',
-            thumbnail: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-            category: 'Article',
-            minRead: '11 min read',
-            publishedDate: '08 Feb, 2022',
-            author: {
-                name: 'Learn with sumit',
-                avatar: 'https://avatars.githubusercontent.com/u/73503432?v=4'
-            }
-        },
-        {
-            id: 3,
             title: 'Improve your customer experience',
             thumbnail: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
             category: 'Explore',
@@ -38,6 +26,18 @@ export const initialState = {
             publishedDate: '20 Mar, 2022',
             author: {
                 name: 'Robert Smith',
+                avatar: 'https://i.ibb.co/bQYqYmQ/comment-3.jpg'
+            }
+        },
+        {
+            id: 3,
+            title: 'How to use search engine optimization to drive sales',
+            thumbnail: 'https://i.ibb.co/xLsnGxx/Untitled-3.png',
+            category: 'Article',
+            minRead: '11 min read',
+            publishedDate: '08 Feb, 2022',
+            author: {
+                name: 'Learn with sumit',
                 avatar: 'https://avatars.githubusercontent.com/u/73503432?v=4'
             }
         },
@@ -50,7 +50,7 @@ export const initialState = {
             publishedDate: '20 Sep, 2022',
             author: {
                 name: 'John Doe',
-                avatar: 'https://avatars.githubusercontent.com/u/73503432?v=4'
+                avatar: 'https://i.ibb.co/6wWBCKK/comment-1.jpg'
             }
         },
         {
@@ -62,7 +62,7 @@ export const initialState = {
             publishedDate: '28 Mar, 2022',
             author: {
                 name: 'John Doe',
-                avatar: 'https://avatars.githubusercontent.com/u/73503432?v=4'
+                avatar: 'https://i.ibb.co/6wWBCKK/comment-1.jpg'
             }
         },
         {
@@ -74,7 +74,7 @@ export const initialState = {
             publishedDate: '21 Jun, 2022',
             author: {
                 name: 'Robert Smith',
-                avatar: 'https://avatars.githubusercontent.com/u/73503432?v=4'
+                avatar: 'https://i.ibb.co/bQYqYmQ/comment-3.jpg'
             }
         },
     ]
